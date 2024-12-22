390308d5b5bc44e1e182120d61c3cdf655659e06
Dhruv Patel <dp14798@gmail.com>
2024-12-21 21:21:37 -0500

    add javascript file

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const proudcts = [
+    { id: 1, name: "product1", price: 29.99 },
+    { id: 2, name: "product2", price: 19.99 },
+    { id: 3, name: "product3", price: 39.99 },
+  ];
+});
