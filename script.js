f517a5cb72842532aa7ddeb1770ddb03381f4d27
Dhruv Patel <dp14798@gmail.com>
2024-12-21 21:30:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ document.addEventListener("DOMContentLoaded", () => {
     { id: 2, name: "product2", price: 19.99 },
     { id: 3, name: "product3", price: 39.99 },
   ];
+
+  const cart = [];
+
+  const productList = document.getElementById("product-list");
+  const cartItems = document.getElementById("cart-items");
+  const emptyCartMessage = document.getElementById("empty-cart");
+  const cartTotalMessage = document.getElementById("cart-total");
+  const totalPriceDisplay = document.getElementById("total-price");
+  const checkOutBtn = document.getElementById("checkout-btn");
+
+  proudcts.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.innerHTML = `
+    <span>
+    ${product.name} - $${product.price.toFixed(2)}
+    </span>`;
+  });
 });
